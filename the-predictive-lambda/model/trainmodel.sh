47d984505f8385dd9ca87e27deda0286dffdbcff
extern.john.nguyen <extern.john.nguyen@vwfs.io>
2021-05-21 13:07:28 +0200

    Add shit

--- the-predictive-lambda/model/trainmodel.sh
+++
@@ -1,5 +0,0 @@
-docker build . -f TrainingDockerfile -t chipotle
-IMAGE_ID=$(docker images -q chipotle)
-docker run -d --name 'training' ${IMAGE_ID} 'app.handler'
-docker cp training:/var/task/chipotle.pkl chipotle.pkl
-docker kill training && docker rm training
